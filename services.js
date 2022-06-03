9befca61d7d82751fc1d9fda8a22ff5b4f221a0c
Xyleff2049 <felixabriac@gmail.com>
2022-06-03 17:33:08 +0200

    Style de la sidenav changé + Script pour la couleur des boutons

--- services.js
+++ services.js
@@ -1,8 +1,16 @@
 var toggle = document.getElementById("toggle");
 var inst = document.getElementsByClassName("inst")[0];
 var sidenav = document.getElementsByClassName("sidenav")[0];
-console.log(toggle);
 toggle.addEventListener("click", function() {
     inst.classList.toggle("inst-active");
     sidenav.classList.toggle("sidenav-active");
 })
+var sidenavButtons = document.getElementsByClassName("sidenav-button");
+function navigate(n) {
+    for (let index = 0; index < sidenavButtons.length; index++) {
+        if (index != n) {
+            sidenavButtons[index].classList.remove("sidenav-button-active");
+        }
+        sidenavButtons[n].classList.add("sidenav-button-active");
+    }
+}
